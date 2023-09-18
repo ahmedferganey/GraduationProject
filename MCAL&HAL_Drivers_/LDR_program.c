d9e4cb24adf954bf8e6dc3525fa93b8a83429b87
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-09-18 10:55:17 +0300

    LDR_StartingPoint

---
+++ MCAL&HAL_Drivers_/LDR_program.c
@@ -0,0 +1,34 @@
+/*
+ * FileName		: LDR_program.c
+ * Created		: 9/18/2023 10:52:51 AM
+ * Author		: Ahmed Ferganey
+ */ 
+ 
+/* -------------------------------- Includes -----------------------------------------------*/
+/**			LIBRARY Includes			**/
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+/**			MCAL Includes				**/
+#include "DIO_interface.h"
+
+/**			HAL Includes				**/
+#include "LDR_interface.h"
+#include "LDR_private.h"
+
+
+
+
+/* -------------------------------- Global Variables ---------------------------------------*/
+
+
+/* -------------------------------- APIs Implementation ------------------------------------*/
+/********************************************************************************************/
+/*  @brief				  : this function uses pooling technique "Sync". @ref port_index_t	*/
+/*							to get the result, TimeoutCounter is used						*/
+/*  @param	 udtPortIndex : to determine the required port				 @ref port_index_t	*/
+/*  @param	 u8Direction  : to Set the required Direction				 @ref uint8			*/
+/*  @return	 Std_ReturnType																	*/
+/*           (E_OK)		  : The function done successfully									*/
+/*           (E_NOT_OK)   : The function has issue to perform this action					*/                                                                   
+/********************************************************************************************/
