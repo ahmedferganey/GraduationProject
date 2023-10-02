7e6015e0243ade83dbe5e8bb7dbb2d4c32a20a44
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-10-02 21:33:12 +0300

    -O Not -o Ya Ahbl

---
+++ 2-APPs/MainApp/build/CMakeFiles/APP.elf.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "APP.elf"
+  "APP.elf.pdb"
+  "CMakeFiles/APP.elf.dir/APP.c.o"
+  "CMakeFiles/APP.elf.dir/APP.c.o.d"
+  "CMakeFiles/APP.elf.dir/src/DIO_program.c.o"
+  "CMakeFiles/APP.elf.dir/src/DIO_program.c.o.d"
+  "CMakeFiles/APP.elf.dir/src/DISPLAY_program.c.o"
+  "CMakeFiles/APP.elf.dir/src/DISPLAY_program.c.o.d"
+  "CMakeFiles/APP.elf.dir/src/LCD_program.c.o"
+  "CMakeFiles/APP.elf.dir/src/LCD_program.c.o.d"
+  "CMakeFiles/APP.elf.dir/src/TIMER_program.c.o"
+  "CMakeFiles/APP.elf.dir/src/TIMER_program.c.o.d"
+  "CMakeFiles/APP.elf.dir/src/ULTRASONIC_program.c.o"
+  "CMakeFiles/APP.elf.dir/src/ULTRASONIC_program.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/APP.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2-APPs/MainApp/build/CMakeFiles/APP.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for APP.elf.
+# This may be replaced when dependencies are built.
