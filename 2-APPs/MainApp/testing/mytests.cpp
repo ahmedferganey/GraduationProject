bccf48155078f2021652f64e241ac204e6b240eb
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-10-04 06:43:00 +0300

    GoogleTest

--- 2-APPs/MainApp/testing/mytests.cpp
+++ 2-APPs/MainApp/testing/mytests.cpp
@@ -11,3 +11,64 @@ int main(int argc, char* argv[])
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+/*
+according to documentatiion can use APIs of google test
+TEST( , )
+{
+    //arrane
+    //call
+    //action
+
+}
+*/
+/*
+Example1 :-
+
+TEST(sumfunction, test_1)
+{
+    EXPECT_EQ(5, sum(2.3));
+    EXPECT_NE(5, sum(2.3));   
+}
+*/
+
+/*
+Example2 :-
+
+TEST(func, test_1)
+{
+    ASSERT_TRUE(func());
+    std::cout << "another test" << std::endl;
+    EXPECT_TRUE(func());
+}
+*/
+
+
+/*
+Example3 :-
+void fun()
+{
+    throw std::logic_error("running time erorr");
+    throw std::runtime_error("running time erorr");
+
+}
+
+TEST(func, test_1)
+{
+    EXPECT_THROW(func(), std::runtimeerror);
+}
+*/
+
+
+/*
+Example4 :-
+const char* fun()
+{
+    return "hello";
+}
+
+TEST(func, test_1)
+{
+    EXPECT_STREQ(func(), "hello");
+}
+*/
