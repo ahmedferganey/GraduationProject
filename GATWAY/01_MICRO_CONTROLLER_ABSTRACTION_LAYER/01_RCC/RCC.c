acb4b024747e9350de583ab450659278fac20928
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-10-12 11:28:04 +0300

    RCC_Driver_

--- GATWAY/01_MICRO_CONTROLLER_ABSTRACTION_LAYER/01_RCC/RCC.c
+++ GATWAY/01_MICRO_CONTROLLER_ABSTRACTION_LAYER/01_RCC/RCC.c
@@ -5,3 +5,51 @@
  * @modify date 2023-10-12 11:02:37
  * @desc [description]
  */
+
+
+#include "BIT_MATH.h"
+#include "Std_Types.h"
+
+#include "RCC_private.h"
+#include "RCC_interface.h"
+#include "RCC_cfg.h"
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                     *******FUNCTIONS DEFINITION******                                  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief 
+/// @param  
+void RCC_VoidInitSysClock(void)
+{
+
+}
+
+/// @brief 
+/// @param Copy_u8BusId 
+/// @param Copy_u8PerId 
+void RCC_VoidEnableClock(uint8 Copy_u8BusId ,  
+                         uint8 Copy_u8PerId   )
+{
+
+}   
+
+/// @brief 
+/// @param Copy_u8BusId 
+/// @param Copy_u8PerId 
+void RCC_VoidDisableClock(uint8 Copy_u8BusId ,  
+                          uint8 Copy_u8PerId   )
+{
+
+}   
+
+/// @brief 
+/// @param Copy_u8BusId 
+/// @param Copy_u8PerId 
+void RCC_VoidResetPeriph(uint8 Copy_u8BusId ,  
+                         uint8 Copy_u8PerId   )
+{
+
+}                         
+
