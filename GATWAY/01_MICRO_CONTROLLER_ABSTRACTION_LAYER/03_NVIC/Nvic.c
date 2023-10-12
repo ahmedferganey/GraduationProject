1ceeead61dd60d06c11f5ef7f508ea5d070febff
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-10-13 00:23:05 +0300

    unwrapping

--- GATWAY/01_MICRO_CONTROLLER_ABSTRACTION_LAYER/03_NVIC/Nvic.c
+++ GATWAY/01_MICRO_CONTROLLER_ABSTRACTION_LAYER/03_NVIC/Nvic.c
@@ -22,25 +22,93 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                     *******FUNCTIONS DEFINITION******                                  //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
+
+/// @brief 
+/// @param  
+void NVIC_voidInit(void)
+{
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+void NVIC_voidEnableIRQ(NVIC_IRQn_t copyIRQn)
+{
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+void NVIC_voidDisableIRQ(NVIC_IRQn_t copyIRQn)
+{
+
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+/// @return 
+uint8   NVIC_u8GetPendingIRQ (NVIC_IRQn_t copyIRQn)
+{
+
+
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+void NVIC_voidSetPendingIRQ (NVIC_IRQn_t copyIRQn)
+{
+
+
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+void NVIC_voidClearPendingIRQ (NVIC_IRQn_t copyIRQn)
+{
+
+
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+/// @return 
+uint8  NVIC_u8GetActive (NVIC_IRQn_t copyIRQn)
+{
+
+
+
+
+
+}
+
+/// @brief 
+/// @param copyIRQn 
+/// @param copy_u8Priority 
+void NVIC_voidSetPriority (NVIC_IRQn_t copyIRQn, uint8 copy_u8Priority)
+{
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
