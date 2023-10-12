34706152c365e879ce65b7cc1d52c783b839b68f
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-10-13 02:08:58 +0300

    Flash_Protection_write_earease

--- GATWAY/01_MICRO_CONTROLLER_ABSTRACTION_LAYER/05_FPEC/Fpec.c
+++ GATWAY/01_MICRO_CONTROLLER_ABSTRACTION_LAYER/05_FPEC/Fpec.c
@@ -26,35 +26,98 @@
 /// @return 
 uint8 FPEC_WriteHalfWord( uint32 copy_u32Address, uint16 copy_u16Data)
 {
+	Std_ReturnType local_u8Error ; 
+	/* Wait if read or write operation is under execution */
+	while (GET_BIT ((FPEC -> FLASH_SR ), BSY ));
+	
+	/* Check on the lock bit */
+	if (GET_BIT ((FPEC -> FLASH_CR ), LOCK )){
+		/* if FPEC locked perform a lock sequence */
+		FPEC -> FLASH_KEYR = FPEC_KEY1 ;
+		FPEC -> FLASH_KEYR = FPEC_KEY2 ;
+	}
+	
+	/* Enable program */
+	SET_BIT ((FPEC -> FLASH_CR ), PG );
+	
+	/* Write byte at the desired place */
+	 (*(uint16 *)copy_u32Address) = copy_u16Data ;
+	
+	/* Wait till the wirte is done */
+	while (GET_BIT ((FPEC -> FLASH_SR ), BSY ));
+	
+	/* Disable PG */
+	CLR_BIT ((FPEC -> FLASH_CR ), PG );
+	
+	/* End of Operation*/
+	SET_BIT ((FPEC -> FLASH_SR ), EOP);
 
-
-
-
-
-
+	/* Check on Data */
+	if ((*(uint16 *)copy_u32Address) == copy_u16Data){
+		local_u8Error = WRITE_DONE ;
+	}
+	else {
+		local_u8Error = WRITE_ERROR ;
+	}
+	
+	return local_u8Error ;
 }
 
 /// @brief 
 /// @param copy_u8PageNumber 
 void FPEC_voidErasePage( uint8 copy_u8PageNumber )
 {
+	/* Wait if read or write operation is under execution */
+	while (GET_BIT ((FPEC -> FLASH_SR ), BSY ));
+	
+	/* Check on the lock bit */
+	if (GET_BIT ((FPEC -> FLASH_CR ), LOCK )){
+		/* if FPEC locked perform a lock sequence */
+		FPEC -> FLASH_KEYR = FPEC_KEY1 ;
+		FPEC -> FLASH_KEYR = FPEC_KEY2 ;
+	}
+	
+	/* Enable erase for page */
+	SET_BIT ((FPEC -> FLASH_CR ), PER );
+	
+	/* Set Address for page */
+	FPEC -> FLASH_AR = FPEC_FLASH_START_ADDRESS + (uint32) (copy_u8PageNumber * FPEC_FLASH_PAGE_SIZE) ;
+	
+	/* Start erase */
+	SET_BIT ((FPEC -> FLASH_CR ), STRT );
 
+	/* Wait till the erase is done */
+	while (GET_BIT ((FPEC -> FLASH_SR ), BSY ));
 
-
-
-
-
+	/* End of Operation*/
+	SET_BIT ((FPEC -> FLASH_SR ), EOP);
+	
+	/* Disable erase for page */
+	CLR_BIT ((FPEC -> FLASH_CR ), PER );
+	
+	/* Lock Flash */
+	SET_BIT ((FPEC -> FLASH_CR ),LOCK);
 }
 
 /// @brief 
 /// @param  
 void FPEC_voidMassErase(void)
 {
+	/* Check on the lock bit */
+	if (GET_BIT ((FPEC -> FLASH_CR ), LOCK )){
+		/* if FPEC locked perform a lock sequence */
+		FPEC -> FLASH_KEYR = FPEC_KEY1 ;
+		FPEC -> FLASH_KEYR = FPEC_KEY2 ;
+	}
+	
+	/* Enable erase for page */
+	SET_BIT ((FPEC -> FLASH_CR ), MER );
+	
+	/* Start erase */
+	SET_BIT ((FPEC -> FLASH_CR ), STRT );
 
-
-
-
-
+	/* Wait till the erase is done */
+	while (GET_BIT ((FPEC -> FLASH_SR ), BSY ));
 }
 
 
