09de6d330cfa275b9afa3204bafa4d93dd027a00
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-10-13 10:12:12 +0300

    BL&LScript&Startupcode

---
+++ BL_STARTUPCODE_LS_STM32f103/startup_stm32f10x.c
@@ -0,0 +1,17 @@
+/**
+ * @author Ahmed Ferganey
+ * @email ahmedferganey707@gmail.com
+ * @file startup_stm32f10x.c
+ * @create date 2023-10-13 09:54:45
+ * @modify date 2023-10-13 09:54:45
+ * @desc [this is startup code for STM32f10x]
+ */
+
+
+
+
+typedef unsigned long uint32_t;
+
+
+extern void main(void);
+
