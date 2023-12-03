eb60cd0a5049a6eddc09e018f7b315b2b96cee94
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-12-03 11:06:47 +0200

    StackAsDatastructure

--- AlgoritmsAndDataStructure/stack/stack.c
+++ AlgoritmsAndDataStructure/stack/stack.c
@@ -18,13 +18,36 @@ void push();
 void pop();
 void show();
 
-
 ///////////////////////////////////////////////////////////////////////////
 int main()
 {
+    u32 Choice = 5;
+
+    while (1)
+    {
+        printf("\nPerform your operations on the stack:");
+        printf("\n1.PUSH    \n2.POP    \n3.SHOW    \n4.END");
+        printf("\nEnter The Choice:");
+        scanf("%d", &Choice);
 
 
-    return 0;
+        switch (Choice)
+        {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                show();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("\ninvalid choice");
+        }
+    }
 }
 ///////////////////////////////////////////////////////////////////////////
 
