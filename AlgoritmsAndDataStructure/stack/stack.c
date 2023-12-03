20e4ec44baa9a5c652896da847273eba4cb9864f
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-12-03 10:56:39 +0200

    StackAsDatastructure

---
+++ AlgoritmsAndDataStructure/stack/stack.c
@@ -0,0 +1,42 @@
+/**
+ * @author Ahmed Ferganey
+ * @email ahmedferganey707@gmail.com
+ * @create date 2023-12-03 10:51:36
+ * @modify date 2023-12-03 10:51:36
+ * @desc [description]
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE    5
+typedef unsigned int u32;
+
+
+void push();
+void pop();
+void show();
+
+
+///////////////////////////////////////////////////////////////////////////
+int main()
+{
+
+
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////
+
+void push()
+{
+
+}
+void pop()
+{
+
+}
+void show()
+{
+
+}
