df2e72d948cb3852e964c45922b13a50a07cc616
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-12-04 11:35:27 +0200

    QueueAsDatastructure

--- AlgoritmsAndDataStructure/queue/queue.c
+++ AlgoritmsAndDataStructure/queue/queue.c
@@ -9,14 +9,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 50
+
 typedef unsigned int uint32;
 typedef signed   int sint32;
 
+void enqueue();
+void dequeue();
+void show();
+
+int Array[SIZE];
 
 
 ///////////////////////////////////////////////////////////////////////////
 int main()
 {
+    uint32 Choice = 5;
+    while(1)
+    {
+        printf("\n1.enqueue");
+        printf("\n2.dequeue");
+        printf("\n3.show");
+        printf("\n4.exit");
+        printf("\nenter your choice");
+        scanf("\n%d",&Choice);
 
+    }
 }
 ///////////////////////////////////////////////////////////////////////////
+void enqueue()
+{
+
+}
+void dequeue()
+{
+
+}
+void show()
+{
+
+}
