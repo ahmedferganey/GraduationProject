997d57a160d27fa0ffb534db94f55683b41688d4
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-12-04 12:09:06 +0200

    QueueAsDatastructure

--- AlgoritmsAndDataStructure/queue/queue.c
+++ AlgoritmsAndDataStructure/queue/queue.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 50
+#define SIZE 4
 
 typedef unsigned int uint32;
 typedef signed   int sint32;
@@ -18,8 +18,9 @@ void enqueue();
 void dequeue();
 void show();
 
-int Array[SIZE];
-
+uint32 Array[SIZE];
+sint32 Front = -1;
+sint32 Rear  = -1;
 
 ///////////////////////////////////////////////////////////////////////////
 int main()
@@ -34,16 +35,62 @@ int main()
         printf("\nenter your choice");
         scanf("\n%d",&Choice);
 
+        switch (Choice)
+        {
+        case 1 :
+            /* code */
+            enqueue();
+            break;
+         case 2 :
+            /* code */
+            dequeue();
+            break;
+        case 3 :
+            /* code */
+            show();
+            break;
+        case 4 :
+            /* code */
+            exit(0);
+            break;
+        default:
+            printf("\ninvalid choice");
+            break;
+        }
     }
 }
 ///////////////////////////////////////////////////////////////////////////
 void enqueue()
 {
+    uint32 InsertedItem;
 
+    if (Rear == SIZE - 1)
+    {
+        printf("\n overflow!!");
+    }
+    else
+    {
+        if (Front == -1)
+        Front = 0;
+
+        printf("\n please enter your data in the queue \n");
+        scanf("%d", &InsertedItem);
+        Rear += 1;
+        Array[Rear] = InsertedItem;
+    }
 }
 void dequeue()
 {
-
+    if (Front == -1 || Front > Rear)
+    {
+        printf("\n underflow!!");
+        return;
+    }
+    else
+    {
+        printf("\nelement deleted from the queue is: %d", Array[Front]);
+        Front += 1;
+    }
 }
 void show()
 {
