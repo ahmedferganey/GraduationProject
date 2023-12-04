9f7190ecbe1495c42076a11796192a289ea98e9d
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-12-04 11:27:34 +0200

    QueueAsDatastructure

---
+++ AlgoritmsAndDataStructure/queue/queue.c
@@ -0,0 +1,22 @@
+/**
+ * @author Ahmed Ferganey
+ * @email ahmedferganey707@gmail.com
+ * @create date 2023-12-04 11:26:36
+ * @modify date 2023-12-04 11:26:36
+ * @desc [description]
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int uint32;
+typedef signed   int sint32;
+
+
+
+///////////////////////////////////////////////////////////////////////////
+int main()
+{
+
+}
+///////////////////////////////////////////////////////////////////////////
