884b0f82f21f051b50307dada9a074cbbcedd64e
ahmedferganey <ahmed.ferganey707@gmail.com>
2023-12-04 12:49:10 +0200

    SingleLinkedListAsDatastructure

---
+++ AlgoritmsAndDataStructure/signlelinkedlist/singlelinkedlist.c
@@ -0,0 +1,96 @@
+/**
+ * @author Ahmed Ferganey
+ * @email ahmedferganey707@gmail.com
+ * @create date 2023-12-04 12:41:28
+ * @modify date 2023-12-04 12:41:28
+ * @desc [description]
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef unsigned int uint32;
+typedef signed   int sint32;
+
+void beginsert ();
+void lastinsert ();
+void begindelete ();
+void lastdelete ();
+void randominsert ();
+void display ();
+
+
+
+///////////////////////////////////////////////////////////////////////////
+int main()
+{
+    uint32 Choice = 10;
+
+    while(1)
+    {
+        printf("\n1.beginsert");
+        printf("\n2.lastinsert");
+        printf("\n3.begindelete");
+        printf("\n4.lastdelete");
+        printf("\n5.randominsert");
+        printf("\n6.display");
+        printf("\n7.exit");       
+        scanf("\n%d",&Choice);
+
+        switch (Choice)
+        {
+        case 1 :
+            /* code */
+            break;
+        case 2 :
+            /* code */
+            break;
+        case 3 :
+            /* code */
+            break;
+        case 4 :
+            /* code */
+            break;
+        case 5 :
+            /* code */
+            break;
+        case 6 :
+            /* code */
+            break;
+        case 7 :
+            /* code */
+            break;
+        default:
+            break;
+        }
+    }
+}
+///////////////////////////////////////////////////////////////////////////
+void beginsert ()
+{
+
+}
+void lastinsert ()
+{
+
+}
+void begindelete ()
+{
+
+}
+void lastdelete ()
+{
+
+}
+void randominsert ()
+{
+
+}
+void display ()
+{
+
+}
+
+
